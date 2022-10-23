2d7a8066ce02d0fb0f289035d4b02587af3e6e29
Karim <karim.summary@gmail.com>
2022-10-23 04:40:46 +0300

    new_2

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -76,8 +76,8 @@ public class UserServiceImpl implements UserService {
         Set<Role> roles2 = new HashSet<>();
         roles2.add(roleDAO.findById(1L).orElse(null));
         roles2.add(roleDAO.findById(2L).orElse(null));
-        User user1 = new User("Boris","Backer",(byte) 25, "user@mail.com", "user","12345",roles1);
-        User user2 = new User("Mao","Dzedun",(byte) 30, "admin@mail.com", "admin","admin",roles2);
+        User user1 = new User("Boris","Backer",(byte) 100, "user@mail.com", "user","12345",roles1);
+        User user2 = new User("Mao","Dzedun",(byte) 100, "admin@mail.com", "admin","admin",roles2);
         save(user1);
         save(user2);
         }
